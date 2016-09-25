3902f15c3fd16ca976c83af3e060ef2729d94ddc
Christian Iversen <ci@iversenit.dk>
2016-09-25 04:01:36 +0200

    + Implemented Flags as a null-variant on Enums

--- cortex/enum.py
+++ cortex/enum.py
@@ -4,7 +4,7 @@ from .strutil import text_width, hex_width
 class Enum(object):
     def __init__(self, header, lines, comment):
         RE_ENUM_FIELD = re.compile("(\w+)\s*=\s*(\w+)")
-        
+
         fields = []
         for line in lines:
             field = RE_ENUM_FIELD.match(line)
@@ -14,7 +14,7 @@ class Enum(object):
         hw = hex_width(fields)
 
         self._name = header
-        self.fields = [EnumCase(f[0], f[1], tw, hw) for f in fields]
+        self.fields = [Case(f[0], f[1], tw, hw) for f in fields]
         self._comment = comment
 
     @property
@@ -29,7 +29,10 @@ class Enum(object):
     def name(self):
         return self._name
 
-class EnumCase(object):
+class Flags(Enum):
+    pass
+
+class Case(object):
     def __init__(self, name, value, name_width, value_width):
         self.name = name
         self.value = value
@@ -43,7 +46,7 @@ class EnumCase(object):
     @property
     def hex_value(self):
         return "0x{:x}".format(self.value)
-    
+
     @property
     def aligned_hex_value(self):
         return "0x{:0{width}x}".format(self.value, width=self._value_width)
