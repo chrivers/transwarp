701d2e508bcfc8a4e86cba83e5d05fbbe1a752ee
Christian Iversen <ci@iversenit.dk>
2016-09-25 12:58:01 +0200

    * Converted struct fields to Field

--- cortex/struct.py
+++ cortex/struct.py
@@ -14,7 +14,7 @@ class Struct(object):
             field = RE_STRUCT_FIELD.match(line)
             if field:
                 name, typ = field.groups()
-                fields.append((name, typ, comment))
+                fields.append(Field(name, typ, comment))
                 comment = []
         self._name = header
         self.fields = fields
@@ -23,3 +23,21 @@ class Struct(object):
     @property
     def name(self):
         return self._name
+
+class Field(object):
+    def __init__(self, name, datatype, cmt):
+        self._name = name
+        self._type = datatype
+        self._cmt = cmt
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def comment(self):
+        return self._cmt
