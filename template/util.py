5fac7d798b3f4ab0ff2d8c1a989cdadfd8fd7aa3
Christian Iversen <ci@iversenit.dk>
2016-09-26 16:42:46 +0200

    * Improved format_comment() algorithm, by making it preserve explicit linebreaks

--- template/util.py
+++ template/util.py
@@ -1,9 +1,14 @@
 import textwrap
 
 def format_comment(comment, indent, width):
-    return textwrap.wrap(
-        "\n".join(comment),
-        initial_indent=indent,
-        subsequent_indent=indent,
-        width=width,
-    )
+    res = []
+    for line in comment:
+        res.extend(
+            textwrap.wrap(
+                line,
+                initial_indent=indent,
+                subsequent_indent=indent,
+                width=width,
+            )
+        )
+    return res
