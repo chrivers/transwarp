d039ee88ecfc7e8b176d43969048023c549af7c2
Christian Iversen <ci@iversenit.dk>
2016-09-26 13:43:37 +0200

    * More experiments with data types

--- cortex/datatype.py
+++ cortex/datatype.py
@@ -2,32 +2,45 @@ import re
 
 class Type(object):
     def __init__(self, text):
-        RE_TYPE = re.compile("(\w+)(?:<(.+)>)?")
+        RE_TYPE = re.compile("(\w+)(?:<(.+)(?:, (.+))?>)?")
         match = RE_TYPE.match(text)
         if not match:
             raise ValueError("Could not parse type [%r]" % text)
-        name, args = match.groups()
+        name, target, arg = match.groups()
         self._name = name
-        self._args = {}
-        if args:
-            if "=" in args[0]:
-                raise ValueError("First type parameter must not be key=value: [%s]" % args[0])
-            self._target = args[0]
-
-        for arg in re.split(",\s*", args or "")[1:]:
-            if "=" not in arg:
-                raise ValueError("All type parameters after the first must be key=value: %r (in %r)" % (arg, text))
-                key, value = arg.split("=", 1)
-                self.args[key.strip()] = value.strip()
+        self._target = Type(target)
+        self._arg = arg
 
     @property
     def name(self):
         return self._name
 
-    @property
-    def target(self):
-        return self._target
+class FixedArray(Type):
+
+    def __init__(self, name, type, length):
+        self._name = name
+        self._type = type
+        self._length = length
 
     @property
-    def arg(self, name, default=None):
-        return self._args.get(name, default)
+    def type(self):
+        return self._type
+
+    @property
+    def length(self):
+        return self._length
+
+class Array(Type):
+
+    def __init__(self, name, type, length):
+        self._name = name
+        self._type = type
+        self._length = length
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def length(self):
+        return self._length
