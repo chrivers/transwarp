e684e9c553f7996f61d5bf3f751972ce16307dd3
Christian Iversen <ci@iversenit.dk>
2016-09-26 15:00:16 +0200

    * Make text_width() and hex_width() handle empty lists

--- cortex/strutil.py
+++ cortex/strutil.py
@@ -4,9 +4,15 @@ MIN_HEX_WIDTH = 2
 def text_width(cases):
     # find the longest string, go with that
     # unless smaller than MIN_TEXT_WIDTH, then go with that instead
-    return max(max(len(case[0]) for case in cases), MIN_TEXT_WIDTH)
+    if cases:
+        return max(max(len(case[0]) for case in cases), MIN_TEXT_WIDTH)
+    else:
+        return 0
 
 def hex_width(cases):
     # find an even number of hex digits that will fit all fields
     # unless smaller than MIN_HEX_WIDTH, then go with that instead
-    return max(max(round(len("%x" % case[1]) / 2.0) * 2 for case in cases), MIN_HEX_WIDTH)
+    if cases:
+        return max(max(round(len("%x" % case[1]) / 2.0) * 2 for case in cases), MIN_HEX_WIDTH)
+    else:
+        return 0
