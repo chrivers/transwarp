0ec7ba59928e0896e03ebb48d6acbd68ebff2988
Christian Iversen <ci@iversenit.dk>
2016-09-26 13:30:48 +0200

    * Experiments with type syntax

--- cortex/datatype.py
+++ cortex/datatype.py
@@ -8,12 +8,26 @@ class Type(object):
             raise ValueError("Could not parse type [%r]" % text)
         name, args = match.groups()
         self._name = name
-        self._args = args
-    
+        self._args = {}
+        if args:
+            if "=" in args[0]:
+                raise ValueError("First type parameter must not be key=value: [%s]" % args[0])
+            self._target = args[0]
+
+        for arg in re.split(",\s*", args or "")[1:]:
+            if "=" not in arg:
+                raise ValueError("All type parameters after the first must be key=value: %r (in %r)" % (arg, text))
+                key, value = arg.split("=", 1)
+                self.args[key.strip()] = value.strip()
+
     @property
     def name(self):
         return self._name
-    
+
     @property
-    def args(self):
-        return self._args
+    def target(self):
+        return self._target
+
+    @property
+    def arg(self, name, default=None):
+        return self._args.get(name, default)
