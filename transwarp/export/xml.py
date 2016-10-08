a838a8a50792b582490b79dd6a2c6bffe006f304
Christian Iversen <ci@iversenit.dk>
2016-10-08 16:22:18 +0200

    * Implemented first full proof-of-concept XML exporter

--- transwarp/export/xml.py
+++ transwarp/export/xml.py
@@ -5,6 +5,42 @@ import lxml.etree as ET
 def dump(elem):
     print(ET.tostring(elem, encoding="unicode", pretty_print=True), end="")
 
+def new_from_obj(obj):
+    tag = ET.Element(obj.__class__.__name__.lower())
+    tag.attrib["name"] = obj.name
+    return tag
+
+def gen_type(type):
+    tag = new_from_obj(type)
+    if type._args:
+        for arg in type._args:
+            tag.append(gen_type(arg))
+    return tag
+
+def gen_enum_or_flag(obj):
+    tag = new_from_obj(obj)
+    for field in obj.fields:
+        ftag = ET.Element("field")
+        ftag.attrib["name"] = field.name
+        ftag.attrib["value"] = field.aligned_hex_value
+        tag.append(ftag)
+    return tag
+
+def gen_struct_field(field):
+    tag = new_from_obj(field)
+    tag.append(gen_type(field.type))
+    return tag
+
+def gen_struct(struct):
+    tag = new_from_obj(struct)
+    for line in struct.comment:
+        tag.append(ET.Comment(line))
+    for field in struct.fields:
+        for line in field.comment:
+            tag.append(ET.Comment(line))
+        tag.append(gen_struct_field(field))
+    return tag
+
 def export(data):
     out = io.StringIO()
     root = ET.Element("root")
@@ -12,20 +48,16 @@ def export(data):
     root.append(ET.Comment(warning))
     for typename, values in data.items():
         for value in values:
-            typetag = ET.Element(typename)
-            typetag.attrib["name"] = value.name
-            root.append(typetag)
             for line in getattr(value, "comment", []):
-                typetag.append(ET.Comment(line))
-            for field in getattr(value, "fields", []):
-                fieldtag = ET.Element("field")
-                for line in getattr(field, "comment", []):
-                    fieldtag.append(ET.Comment(line))
-                fieldtag.attrib["name"] = field.name
-                if hasattr(field, "type"):
-                    fieldtag.attrib["type"] = str(field.type)
-                if hasattr(field, "value"):
-                    fieldtag.attrib["value"] = field.value
-                typetag.append(fieldtag)
+                root.append(ET.Comment(line))
+            if typename == "packet":
+                for field in value.fields:
+                    root.append(gen_struct(field))
+            elif typename in ("enum", "flags"):
+                root.append(gen_enum_or_flag(value))
+            elif typename in ("struct", "parser", "object"):
+                root.append(gen_struct(value))
+            else:
+                print("Unknown type tag: %s" % typename)
     root.append(ET.Comment(warning))
     dump(root)
