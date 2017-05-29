ce0dd6c0fe06d0e36e5bf1e0ce0dd8da7c6eec0f
Christian Iversen <ci@iversenit.dk>
2017-05-29 23:41:45 +0200

    + Implemented SearchableList.__contains__ for better ergonomics

--- transwarp/util/data.py
+++ transwarp/util/data.py
@@ -1,5 +1,11 @@
 class SearchableList(list):
 
+    def __contains__(self, name):
+        for x in self:
+            if x.name == name:
+                return True
+        return False
+
     def get(self, name, default=...):
         for x in self:
             if x.name == name:
