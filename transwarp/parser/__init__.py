496140a9edd03c09ed438aac780af0fd5081be2c
Christian Iversen <ci@iversenit.dk>
2017-05-29 16:01:59 +0200

    * Completely rewrote transwarp parser, to implement new gamma syntax specification

--- transwarp/parser/__init__.py
+++ transwarp/parser/__init__.py
@@ -1,58 +1,107 @@
-import fileinput
-from transwarp.parser.file import File
-from transwarp.parser.grammar import RE_PARSER_DEF, RE_STRUCT_FIELD, RE_DOC, RE_BLANK, RE_BLOCK, RE_FIELD
+import os
+import logging as log
+from transwarp.util.data import SearchableList
+from transwarp.parser.field import Field
+from transwarp.parser.const import Const
+from transwarp.parser.type import Type
+from transwarp.parser.block import Block
+from transwarp.parser.grammar import RE_DOC, RE_BLANK, RE_BLOCK_START, RE_BLOCK_FIELD, RE_BLOCK_CONST
 
 class Parser(object):
 
     def __init__(self, lines):
         self.lines = lines
+        self._lastline = None
+
+    @property
+    def line(self):
+        return self._lastline
 
     def nextline(self):
         try:
-            return next(self.lines)
+            self._lastline = next(self.lines)
         except StopIteration:
-            return ""
+            self._lastline = ""
+        return self._lastline
 
-    def parse_file(self):
-        blocks = []
-        comment = []
-        line = self.nextline()
-        while line:
-            if RE_BLANK.match(line):
-                line = self.nextline()
+    def parse_block(self, name=None, expr=None, arg=None, comment=None, level=0):
+        self.nextline()
+        blocks = SearchableList()
+        fields = SearchableList()
+        consts = SearchableList()
+        nextcomment = []
+        while self.line:
+            if RE_BLANK.match(self.line):
+                # print("BLANK: [%r]" % self.line)
+                self.nextline()
                 continue
-            docline = RE_DOC.match(line)
+
+            docline = RE_DOC.match(self.line)
             if docline:
-                comment.append(docline.group(1).strip())
-                line = self.nextline()
+                # print("DOC: %r" % (docline.groups(), ))
+                curlevel = len(docline.group(1) or "") / 4
+                if curlevel != level and level and curlevel:
+                    break
+                nextcomment.append(docline.group(2).strip())
+
+                self.nextline()
                 continue
 
-            block = RE_BLOCK.search(line)
+            field = RE_BLOCK_FIELD.match(self.line)
+            if field:
+                # print("FIELD: %r" % (field.groups(), ))
+                curlevel = len(field.group(1) or "") / 4
+                if curlevel != level and level and curlevel:
+                    break
+                fields.append(Field(field.group(2), Type.parse(field.group(3)), comment=nextcomment))
+                nextcomment = []
+
+                self.nextline()
+                continue
+
+            const = RE_BLOCK_CONST.match(self.line)
+            if const:
+                # print("CONST: %r" % (const.groups(), ))
+                curlevel = len(const.group(1) or "") / 4
+                if curlevel != level and level and curlevel:
+                    break
+                consts.append(Const(const.group(2), Const.parse(const.group(3)), comment=nextcomment))
+                nextcomment = []
+
+                self.nextline()
+                continue
+
+            block = RE_BLOCK_START.match(self.line)
             if block:
-                block_comment = comment[:]
-                block_lines = []
-                comment = []
-                while line:
-                    line = self.nextline()
-                    if RE_BLANK.match(line):
-                        continue
-
-                    field = RE_FIELD.match(line)
-                    if field:
-                        block_lines.append(field.group(1))
-                    else:
-                        typ, header = block.groups()
-                        blocks.append((typ, header, block_lines, block_comment))
-                        break
+                # print("BLOCK: %r" % (block.groups(), ))
+                curlevel = len(block.group(1) or "") / 4
+                if curlevel != level and level:
+                    break
+                if block.group(4):
+                    arg = Type.parse(block.group(4))
+                else:
+                    arg = Type("arg", [])
+                blocks.append(self.parse_block(
+                    name=block.group(3),
+                    expr=block.group(2),
+                    arg=arg,
+                    comment=nextcomment,
+                    level=level + 1
+                ))
+                nextcomment = []
                 continue
-            else:
-                raise ValueError("Unknown line: %r" % line)
-        return blocks
 
-from pprint import pprint
+            raise ValueError("Parse error for line: %r" % self.line)
+
+        blocks.sort()
+        return Block(name, expr, blocks, fields, consts, arg, comment)
+
 def parse(files):
-    lines = fileinput.input(files=files)
-    parser = Parser(lines)
-    pprint(parser.parse_file())
-
-    return {}
+    res = SearchableList()
+    for name in sorted(files):
+        log.debug("Parsing file [%r]" % name)
+        lines = open(name)
+        parser = Parser(lines)
+        ident = os.path.splitext(os.path.basename(name))[0]
+        res.append(parser.parse_block(name=ident, expr=name))
+    return res
