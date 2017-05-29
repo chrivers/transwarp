0396f707888e4a04910c552fe4a6a931434828f1
Christian Iversen <ci@iversenit.dk>
2017-05-30 00:10:25 +0200

    + Re-implemented SearchableList from object instead of list, to allow better control over interface and data model. This allows hiding attribute values, which should not be iterated by default

--- transwarp/util/data.py
+++ transwarp/util/data.py
@@ -1,19 +1,39 @@
-class SearchableList(list):
+class SearchableList(object):
+
+    def __init__(self, elms=None):
+        self.data = elms or []
+
+    def append(self, val):
+        self.data.append(val)
+
+    def sort(self):
+        self.data.sort()
+
+    def __bool__(self):
+        return len(list(self)) > 0
 
     def __contains__(self, name):
-        for x in self:
+        for x in self.data:
             if x.name == name:
                 return True
         return False
 
+    def __getitem__(self, idx):
+        return self.data[idx]
+
     def get(self, name, default=...):
-        for x in self:
+        for x in self.data:
             if x.name == name:
                 return x
         if default == ...:
             raise KeyError("Could not find element with name [%r]" % (name, ))
         else:
             return default
+
+    def __iter__(self):
+        for elm in self.data:
+            if not (elm.name.startswith("__") and elm.name.endswith("__")):
+                yield elm
 
     def without(self, *names):
         res = []
