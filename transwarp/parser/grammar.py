432d579dba727c41e14c37f992f354bf2a5909f4
Christian Iversen <ci@iversenit.dk>
2017-05-29 15:40:22 +0200

    + Updated grammar rules for gamma syntax, where all blocks are generically unified

--- transwarp/parser/grammar.py
+++ transwarp/parser/grammar.py
@@ -1,10 +1,9 @@
 import re
 
 RE_BLANK        = re.compile("^\s+$|^\s*##.*")
-RE_DOC          = re.compile("^#([^#]?.*)$")
-RE_BLOCK        = re.compile("^(\w+)\s*(.*)$")
-RE_FIELD        = re.compile("^    (.*)")
-RE_ENUM_FIELD   = re.compile("(\w+)\s*=\s*(\w+)")
-RE_STRUCT_FIELD = re.compile("(\w+):\s*(.*)")
+RE_DOC          = re.compile("^((?:    )*)#([^#]?.*)$")
+RE_BLOCK_START  = re.compile("^((?:    )*)(\w+)\s+(\w+)(?:\((.+)\))?$")
+RE_BLOCK_CONST  = re.compile("^((?:    )*)(\w+)\s*=\s*(.*)")
+RE_BLOCK_FIELD  = re.compile("^((?:    )*)(\w+)\s*:\s*(.*)")
 RE_TYPE         = re.compile("([\w:.]+)(?:<(.+)>)?")
-RE_PARSER_DEF   = re.compile("(\w+)\((.+)\)$")
+RE_IDENT_SPLIT  = re.compile("(\w+)(::|\.)(.*)")
