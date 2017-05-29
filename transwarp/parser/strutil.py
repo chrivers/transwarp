2ac0165a62d29f2b177f2f944552c3bfe7b156b1
Christian Iversen <ci@iversenit.dk>
2017-05-29 15:39:21 +0200

    * Updated strutil to work with new const/field types

--- transwarp/parser/strutil.py
+++ transwarp/parser/strutil.py
@@ -5,7 +5,7 @@ def text_width(cases):
     # find the longest string, go with that
     # unless smaller than MIN_TEXT_WIDTH, then go with that instead
     if cases:
-        return max(max(len(case[0]) for case in cases), MIN_TEXT_WIDTH)
+        return max(max(len(case.name) for case in cases), MIN_TEXT_WIDTH)
     else:
         return 0
 
@@ -13,6 +13,6 @@ def hex_width(cases):
     # find an even number of hex digits that will fit all fields
     # unless smaller than MIN_HEX_WIDTH, then go with that instead
     if cases:
-        return max(max(round(len("%x" % case[1]) / 2.0) * 2 for case in cases), MIN_HEX_WIDTH)
+        return max(max(round(len("%x" % case.expr) / 2.0) * 2 for case in cases), MIN_HEX_WIDTH)
     else:
         return 0
