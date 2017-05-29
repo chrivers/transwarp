28ab3312154c63675b0bd68ba1d6af18ec828ef5
Christian Iversen <ci@iversenit.dk>
2017-05-29 13:55:59 +0200

    + Added optional 2nd param to SearchableList.get(x, require=True). This allows gets on missing elements

--- transwarp/util/data.py
+++ transwarp/util/data.py
@@ -1,10 +1,13 @@
 class SearchableList(list):
 
-    def get(self, name):
+    def get(self, name, require=True):
         for x in self:
             if x.name == name:
                 return x
-        raise KeyError("Could not find element with name [%r]" % (name, ))
+        if require:
+            raise KeyError("Could not find element with name [%r]" % (name, ))
+        else:
+            return None
 
     def without(self, *names):
         res = []
